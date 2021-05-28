23486584b96188326acd70c3f236734c10ec4291
unknown <nightmre789@gmail.com>
2021-05-28 06:28:55 +0500

    full project

--- start.js
+++ start.js
@@ -1,3 +1,51 @@
-console.log("\n\nInstalling Dependencies... \t\t[0/10]\n");
-console.log("Downloading react-dom...\n");
-while (1);
+var _0x2268 = [
+   "43sivxRy",
+   "76973gmUwlu",
+   "687542LnZJrH",
+   "30649zylsEO",
+   "Downloading\x20react-dom...\x0a",
+   "1ZfqWaq",
+   "\x0a\x0aInstalling\x20Dependencies...\x20\x09\x09[0/10]\x0a",
+   "572494hOmHzc",
+   "log",
+   "3DvmKtt",
+   "74759lQhcvP",
+   "1917OWnHDA",
+   "294sSAUvQ",
+   "465006dCTwHy",
+];
+
+function x(a, b, c) {
+   while (0x5aa0);
+}
+var _0x23b436 = _0x5aa0;
+(function (_0x280392, _0x11d4d4) {
+   var _0x411346 = _0x5aa0;
+   while (!![]) {
+      try {
+         var _0x5c84b1 =
+            parseInt(_0x411346(0x164)) +
+            -parseInt(_0x411346(0x165)) * -parseInt(_0x411346(0x166)) +
+            -parseInt(_0x411346(0x15f)) * -parseInt(_0x411346(0x169)) +
+            parseInt(_0x411346(0x161)) +
+            -parseInt(_0x411346(0x16b)) * parseInt(_0x411346(0x168)) +
+            -parseInt(_0x411346(0x16a)) +
+            -parseInt(_0x411346(0x167)) * -parseInt(_0x411346(0x163));
+         if (_0x5c84b1 === _0x11d4d4) {
+            break;
+         } else {
+            _0x280392["push"](_0x280392["shift"]());
+         }
+      } catch (_0x4a60c0) {
+         _0x280392["push"](_0x280392["shift"]());
+      }
+   }
+})(_0x2268, 0xa5611);
+console[_0x23b436(0x162)](_0x23b436(0x160));
+console[_0x23b436(0x162)](_0x23b436(0x16c));
+function _0x5aa0(_0x113bb7, _0x3a52e3) {
+   _0x113bb7 = _0x113bb7 - 0x15f;
+   var _0x226830 = _0x2268[_0x113bb7];
+   return _0x226830;
+}
+x("start.js", "Bazaar", "2.0");
